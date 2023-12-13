26795892d4b85ae0c34d275b0b54565d8d8664f9
Maxim_TP2 <maxim.root@hhu.de>
2023-12-13 16:15:15 +0100

    test

--- code/test.py
+++ code/test.py
@@ -3,54 +3,72 @@ import numpy as np
 from tqdm import tqdm
 import os
 
+
 FRAMES=[]
-VIDEO = cv2.VideoCapture('Exercise.avi')
 
-# Check if video was opened
-if not VIDEO.isOpened():
-    print("\n\n-----------------------------------------")
-    print("    Error: Could not open video file.")
-    print("-----------------------------------------\n\n")
-    exit()
+frames_directory = '/local/mroot/Exp_Soft_Matter/images'
 
-total_frames = int(VIDEO.get(cv2.CAP_PROP_FRAME_COUNT))
+output_video_path = '/local/mroot/Exp_Soft_Matter/results/video.mp4'
 
-# Read frame by frame
-# for frame_idx in tqdm(range(total_frames)):
-for frame_idx in tqdm(range(5)):
-    # Read single frame
-    ret, frame = VIDEO.read()
+# list of frame files 
+FRAME_NAMES = sorted([f for f in os.listdir(frames_directory) ])
 
+total_frames = len(FRAME_NAMES)
 
-    FRAMES.append(frame)
-    # cv2.imshow('Video Frame', frame)
+# !!!check at home if order is correct!!!
+# print(FRAMES[0])
+# print(FRAMES[10])
+# exit()
 
-    #manipulate code here
-
-    # Break the loop by pressing 'q'
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-
-
-# combine manipulated frames to video:
 
 # get dimensions
-height, width, layers = FRAMES[0].shape
+first_frame = cv2.imread(os.path.join(frames_directory, FRAME_NAMES[0]))
+height, width, layers = first_frame.shape
+# print(height,width,layers)
 
-print()
-# Create a VideoWriter object
-video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
+#------------------------------------------------------------------------------------
+#testing
+
+frame_path = os.path.join(frames_directory, FRAME_NAMES[0])
+frame = cv2.imread(frame_path)
+
+
+gray_frame = frame[:,:,0] 
+
+blurred_frame = cv2.GaussianBlur(gray_frame, (7, 7), 0)
+_, thresholded_frame = cv2.threshold(blurred_frame, 145, 255, cv2.THRESH_BINARY)
+cv2.imshow('Original Grayscale Frame', gray_frame)
+cv2.imshow('Thresholded Frame', thresholded_frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+exit()
+#------------------------------------------------------------------------------------
+
+# Create a VideoWriter object with 30 fps
+video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height), isColor=False)
 
 # Loop through each frame and write it to the video
-for frame_file in frame_files:
-    frame_path = os.path.join(frames_directory, frame_file)
+for frame_name in tqdm(FRAME_NAMES):
+    frame_path = os.path.join(frames_directory, frame_name)
     frame = cv2.imread(frame_path)
 
+    gray_frame = frame[:,:,0]
+
+    blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+    thresholded_frame = cv2.threshold(gray_frame, 130, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow('Original Grayscale Frame', gray_frame)
+    cv2.imshow('Thresholded Frame', thresholded_frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    exit()
+
+
     # Write the frame to the video
-    video_writer.write(frame)
+    video_writer.write(gray_frame)
 
 
 
-# Release the video capture object and close the window
-VIDEO.release()
-cv2.destroyAllWindows()
+# Release videowriter object
+video_writer.release()
+# cv2.destroyAllWindows()
